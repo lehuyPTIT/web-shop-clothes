1b3c874271d53aeb1e59d3eb2fcd5f8da74503bb
lehuy <huyhuys297@gmail.com>
2020-03-01 17:32:30 +0700

    change port

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3001;
+
 var mongoose=require('mongoose');
 var bodyParser = require('body-parser');
 var cookieParser = require('cookie-parser')
 
 require('dotenv').config();
-
+const port = process.env.PORT||3001;
 var homeRoutes =require('./routes/home.route');
 var authRoutes =require('./routes/auth.route');
 var cartRoutes=require('./routes/cart.route');
@@ -18,7 +18,7 @@ app.use(bodyParser.json()) // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 app.use(cookieParser('asda732asd'));
 app.use(sessionMiddleware);
-mongoose.connect(process.env.DB_HOST);
+mongoose.connect(process.env.MONGO_URL);
 
 app.set('view engine', 'pug');
 app.set('views', './views');
